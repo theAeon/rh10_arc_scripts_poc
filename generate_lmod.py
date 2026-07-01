d887ece621026eb37e4e21d2c33093d0f273e31c
Andrew Robbins <andrew@robbinsa.me>
2026-07-01 13:46:09 -0400

    detect and replace paths in templates

---
+++ generate_lmod.py
@@ -0,0 +1,28 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "mako>=1.3.12",
+# ]
+# ///
+
+import glob
+import pathlib
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
+
+def main() -> None:
+    modlist = glob.glob("**/*.lua.tmpl", recursive=True)
+    prefix_root = "/sw/arc/pkgs"
+    dirs = TemplateLookup(directories=["./"])
+    for pathstr in modlist:
+        pathpath = pathlib.Path(pathstr[:-5])  # remove .tmpl suffix
+        program_ver = pathpath.stem
+        program_name = pathpath.parent.stem
+        base = Template(filename=pathstr, lookup=dirs)
+        print(base.render(root=prefix_root,
+              ver=program_ver, name=program_name))
+
+
+if __name__ == "__main__":
+    main()
