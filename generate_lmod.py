6ba1479c22028761323b55ed66f55e77a9c14775
Andrew Robbins <andrew@robbinsa.me>
2026-07-02 13:52:29 -0400

    actually generate the lmods

--- generate_lmod.py
+++ generate_lmod.py
@@ -8,8 +8,8 @@
 import pathlib
 import sys
 from argparse import ArgumentParser
-from mako.template import Template
-from mako.lookup import TemplateLookup
+from mako.template import Template  # type: ignore
+from mako.lookup import TemplateLookup  # type: ignore
 
 
 def main() -> None:
@@ -17,14 +17,18 @@ def main() -> None:
     parser.add_argument('script', help='Path to the build script', nargs='+')
     args = parser.parse_args(sys.argv[1:])
     prefix_root = "/sw/arc/pkgs"
+    module_root = "/sw/arc/modules"
     dirs = TemplateLookup(directories=["./"])
     for pathstr in args.script:
         pathpath = pathlib.Path(pathstr)
         program_ver = pathpath.name
         program_name = pathpath.parent.stem
-        base = Template(filename=f'{pathstr}.lua.mako', lookup=dirs)
-        print(base.render(root=prefix_root,
-              ver=program_ver, name=program_name))
+        base = Template(filename=f'{pathstr}.lua.mako', lookup=dirs,
+                        input_encoding='utf-8', output_encoding='utf-8')
+        (pathlib.Path(module_root) / pathpath.parent).mkdir(parents=True, exist_ok=True)
+        with open(f"{module_root}/{pathstr}.lua", "wb") as f:
+            f.write(base.render(root=prefix_root,
+                                ver=program_ver, name=program_name))  # type: ignore
 
 
 if __name__ == "__main__":
