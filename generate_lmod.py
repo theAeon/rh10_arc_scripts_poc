9ca37366f71f6023ed0e88204609872882204596
Andrew Robbins <andrew@robbinsa.me>
2026-07-02 13:01:48 -0400

    render argument-passed rather than everything

--- generate_lmod.py
+++ generate_lmod.py
@@ -5,21 +5,24 @@
 # ]
 # ///
 
-import glob
 import pathlib
+import sys
+from argparse import ArgumentParser
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
 
 def main() -> None:
-    modlist = glob.glob("**/*.lua.mako", recursive=True)
+    parser = ArgumentParser()
+    parser.add_argument('script', help='Path to the build script', nargs='+')
+    args = parser.parse_args(sys.argv[1:])
     prefix_root = "/sw/arc/pkgs"
     dirs = TemplateLookup(directories=["./"])
-    for pathstr in modlist:
-        pathpath = pathlib.Path(pathstr[:-5])  # remove .mako suffix
-        program_ver = pathpath.stem
+    for pathstr in args.script:
+        pathpath = pathlib.Path(pathstr)
+        program_ver = pathpath.name
         program_name = pathpath.parent.stem
-        base = Template(filename=pathstr, lookup=dirs)
+        base = Template(filename=f'{pathstr}.lua.mako', lookup=dirs)
         print(base.render(root=prefix_root,
               ver=program_ver, name=program_name))
 
